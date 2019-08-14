b0b9710393bc4c27506077fbb394c80ec05d21c3
mertkolgu <mertkolgu@outlook.com>
2019-08-14 19:42:07 +0300

    encapsulation

---
+++ src/com/javase/_09/encapsulation/NotesForEncapsulation.java
@@ -0,0 +1,13 @@
+package com.javase._09.encapsulation;
+
+public class NotesForEncapsulation {
+    //
+}
+
+// encapsulation -> sarmalama/kapsülleme gibi anlamlara gelmektedir.
+
+// 1 - instance değişkenleri private/protected tanımla.
+// 2 - bu instance değişkenlere erişim public getter/setter metodları ile olsun.
+// 3 - bu getter/setter metodların isimleri standartlara uysun.
+
+// bu 3 kurala uyduğumuzda tanımladığımız sınıf encapsulation'a uygun hale gelir.
