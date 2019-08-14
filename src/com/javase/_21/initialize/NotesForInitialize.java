db9bb8344ccc781737abe0fc0b48205199315de8
mertkolgu <mertkolgu@outlook.com>
2019-08-14 19:59:40 +0300

    initialize

---
+++ src/com/javase/_21/initialize/NotesForInitialize.java
@@ -0,0 +1,27 @@
+package com.javase._21.initialize;
+
+import java.util.Arrays;
+
+public class NotesForInitialize {
+
+    // instance ve static değişkenlere otomatik olarak değerler assign edilir.
+
+    int intVariable;
+    byte byteVariable;
+    boolean booleanVariable;
+    double doubleVariable;
+    String stringVariable;
+    int[] arrayVariable;
+
+    public static void main(String[] args) {
+
+        NotesForInitialize init = new NotesForInitialize();
+        System.out.println(init);
+    }
+
+    @Override
+    public String toString() {
+        return "NotesForInitialize [intVariable = " + intVariable + ", byteVariable = " + byteVariable + ", booleanVariable = " + booleanVariable
+                + ", doubleVariable = " + doubleVariable + ", stringVariable = " + stringVariable + ", arrayVariable = " + Arrays.toString(arrayVariable) + "]";
+    }
+}
