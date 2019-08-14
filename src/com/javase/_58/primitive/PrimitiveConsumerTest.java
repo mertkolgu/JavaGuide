6d0b2f1373f16fe7f4437bc336e680c6e3f973a5
mertkolgu <mertkolgu@outlook.com>
2019-08-14 21:04:51 +0300

    primitive

---
+++ src/com/javase/_58/primitive/PrimitiveConsumerTest.java
@@ -0,0 +1,26 @@
+package com.javase._58.primitive;
+
+public class PrimitiveConsumerTest {
+    //
+}
+
+// public interface Consumer<T> {
+// void accept(T t);
+
+// IntConsumer
+// void accept(int value);
+
+// LongConsumer
+// void accept(long value);
+
+// DoubleConsumer
+// void accept(double value);
+
+// ObjIntConsumer<T>
+// void accept(T t, int value);
+
+// public interface ObjLongConsumer<T> {
+// void accept(T t, long value);
+
+// public interface ObjDoubleConsumer<T> {
+// void accept(T t, double value);
---
+++ src/com/javase/_58/primitive/PrimitiveFunctionTest.java
@@ -0,0 +1,25 @@
+package com.javase._58.primitive;
+
+import java.util.function.Function;
+import java.util.function.IntFunction;
+
+public class PrimitiveFunctionTest {
+
+    public static void main(String[] args) {
+
+        // public interface Function<T, R> {
+        // R apply(T t);
+
+        // public interface IntFunction<R> {
+        // R apply(int value);
+
+        // public interface LongFunction<R> {
+        // R apply(long value);
+
+        // public interface DoubleFunction<R> {
+        // R apply(double value);
+
+        Function<Integer, Integer> absInt = Math::abs;
+        IntFunction<Integer> absIntV2 = Math::abs;
+    }
+}
