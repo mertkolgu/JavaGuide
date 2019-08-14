15e0f83f344e1f460ecc1eaa7f9ef25fa7560ad8
mertkolgu <mertkolgu@outlook.com>
2019-08-14 20:10:06 +0300

    loops

---
+++ src/com/javase/_30/loops/LoopTest.java
@@ -0,0 +1,12 @@
+package com.javase._30.loops;
+
+public class LoopTest {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 50, 33, 45, 646};
+
+        for (int e : numbers)
+            System.out.println(e);
+    }
+}
---
+++ src/com/javase/_30/loops/NotesForLoops.java
@@ -0,0 +1,15 @@
+package com.javase._30.loops;
+
+public class NotesForLoops {
+    //
+}
+
+// while
+// for
+// do while
+
+// iç içe çok fazla döngü kullanmayın
+// iç içe 3 ya da 4 tane döngü yapıyorsanız burada bir sıkıntı olabilir.
+// bu noktada koda bakın, algoritmanızı değiştirmeye çalışın.
+
+// sonsuz döngülere DİKKAT
