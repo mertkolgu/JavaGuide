b2e604653995a887a8f41a0a14eade3359c25831
mertkolgu <mertkolgu@outlook.com>
2019-08-14 21:37:47 +0300

    atomic

---
+++ src/com/javase/_78/atomic/AtomicTest01.java
@@ -0,0 +1,27 @@
+package com.javase._78.atomic;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicTest01 {
+
+    public static void main(String[] args) {
+
+        AtomicInteger atomic = new AtomicInteger();
+        atomic.set(10);
+        atomic.getAndIncrement();   // c++;
+        atomic.incrementAndGet();   // ++c;
+
+        System.out.println(atomic);
+
+        // The classes such as AtomicInteger do not use a lock; rather, they
+        // internally use volatile variables and a low-level mechanism known as
+        // Compare-And-Set (CAS). For this reason, using AtomicInteger and
+        // related classes is faster than using locks using synchronized
+        // keyword.
+    }
+}
+
+// c++ ->
+// 1 - c'nin değerini oku
+// 2 - c'yi bir artır
+// 3 - c'nin güncel değerini yaz!
