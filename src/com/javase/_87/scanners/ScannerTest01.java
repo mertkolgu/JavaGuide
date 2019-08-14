75c76714eab0f01e5ea7efd79d14f807472b9ada
mertkolgu <mertkolgu@outlook.com>
2019-08-14 21:47:44 +0300

    scanner

---
+++ src/com/javase/_87/scanners/ScannerTest01.java
@@ -0,0 +1,16 @@
+package com.javase._87.scanners;
+
+import java.util.Scanner;
+
+public class ScannerTest01 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Lütfen Adınızı ve Soyadınızı Giriniz : ");
+        String name = sc.nextLine();
+
+        System.out.println("Adınız : " + name);
+    }
+}
