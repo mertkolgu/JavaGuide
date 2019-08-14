7b247cd0c9c980649c4203b455b0796cd3f0aeb7
mertkolgu <mertkolgu@outlook.com>
2019-08-14 20:01:10 +0300

    arrays

---
+++ src/com/javase/_23/arrays/ArrayMultiDimension.java
@@ -0,0 +1,16 @@
+package com.javase._23.arrays;
+
+public class ArrayMultiDimension {
+
+    public static void main(String[] args) {
+
+        int[][] myArray = new int[3][];
+        // 2-D array tanımladık.
+        // elemanları -> int[]
+        // elemanları int[] olduğu için -> null atanır!
+
+        int[] oneDimension = new int[3];
+        // elemanları -> int tipindedir.
+        // elemanları int olduğu için otomatik olarak 0 atanır!
+    }
+}
