21457c9da483b898f58568a9e2c5ea88201b272a
mertkolgu <mertkolgu@outlook.com>
2019-08-14 21:43:10 +0300

    fork and join

---
+++ src/com/javase/_85/forkandjoin/ForkAndJoinTest02.java
@@ -0,0 +1,16 @@
+package com.javase._85.forkandjoin;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+
+public class ForkAndJoinTest02 {
+
+    public static void main(String[] args) {
+
+        Double[] weights = new Double[10];
+        ForkJoinTask<Double> task = new WeighAnimalRecursiveTask(weights, 0, weights.length);
+        ForkJoinPool pool = new ForkJoinPool();
+        Double sum = pool.invoke(task);
+        System.out.println("Sum: " + sum);
+    }
+}
