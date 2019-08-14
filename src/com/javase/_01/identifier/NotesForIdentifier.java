1c8e898192f3e480af597bc6a286068b46cd3c25
mertkolgu <mertkolgu@outlook.com>
2019-08-14 19:22:44 +0300

    identifier

---
+++ src/com/javase/_01/identifier/NotesForIdentifier.java
@@ -0,0 +1,26 @@
+package com.javase._01.identifier;
+
+public class NotesForIdentifier {
+    //
+}
+
+// legal identifier
+// legal -> code compile olur.
+// identifier -> belirteç, belirleyici gibi anlamlara gelmektedir.
+
+// class, variable, method her ögenin bir isme ihtiyacı vardır
+// bu isme identifier adı verilmektedir.
+
+// ilk karakter rakam olamaz, sonrasındaki karakterler rakam olabilir.
+
+// Genel olarak harf, underscore ( _ ), $ kullanılmaktadır.
+
+// Java'da keyword'ler identifier ismi olarak kullanılamazlar.
+
+// identifier isimleri case sensitive(büyük/küçük harf duyarlı)dir.
+
+// int number ile int NUMBER farklı değişkenlerdir.
+
+// identifier ismi olarak uzunluk açısından bir sınır yoktur.
+
+// # . , ; - gibi karakterler kullanılamazlar.
