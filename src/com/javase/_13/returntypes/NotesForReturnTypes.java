c98bed90d91b85ad2905ab1681f2530c83655a41
mertkolgu <mertkolgu@outlook.com>
2019-08-14 19:48:50 +0300

    return types

---
+++ src/com/javase/_13/returntypes/NotesForReturnTypes.java
@@ -0,0 +1,34 @@
+package com.javase._13.returntypes;
+
+public class NotesForReturnTypes {
+    //
+}
+
+// covariant return type
+// override metotlarda dönüş tipi değişmez.
+// fakat covariant return type olabilir!
+
+class Computer {
+    //
+}
+
+class Laptop extends Computer {
+    //
+}
+
+class Store {
+
+    public Computer buyComputer() {
+        return new Computer();
+    }
+}
+
+class EStore extends Store {
+
+    @Override
+    // override metotlarda dönüş tipi subclass olabilir.
+    // covariant return type adı verilir.
+    public Laptop buyComputer() {
+        return new Laptop();
+    }
+}
