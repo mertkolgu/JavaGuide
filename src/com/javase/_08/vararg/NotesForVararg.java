49f85052e5e90317002a780758615df22a2c0cb3
mertkolgu <mertkolgu@outlook.com>
2019-08-14 19:40:43 +0300

    var arg

---
+++ src/com/javase/_08/vararg/NotesForVararg.java
@@ -0,0 +1,16 @@
+package com.javase._08.vararg;
+
+public class NotesForVararg {
+    //
+}
+
+// var-arg
+// bir method ...
+// 0 tane değer geçebilir.
+// 1 tane ...
+// 2 ...
+// n tane değer geçebilmek istiyorum.
+
+// int ... vararg
+// String ... vararg
+// şeklinde tanımlanmakta.
