cda7a92162cd0a9c182da72b969f6223cc9dd66e
mertkolgu <mertkolgu@outlook.com>
2019-08-14 19:32:18 +0300

    declare interface

---
+++ src/com/javase/_04/declareinterface/MyInterface.java
@@ -0,0 +1,9 @@
+package com.javase._04.declareinterface;
+
+public interface MyInterface {
+
+    void testMethod();  // abstract method
+}
+
+// Java 8'e kadar interface'lerde sadece abstract methodlar tanımlanabiliyordu.
+// Java 8 ile birlikte artık static ve default methodlar tanımlanabilir.
---
+++ src/com/javase/_04/declareinterface/NotesForInterface.java
@@ -0,0 +1,9 @@
+package com.javase._04.declareinterface;
+
+public class NotesForInterface {
+    //
+}
+
+// interface, implements
+// interface'in methodları public abstract'tır!
+// interface'in değişkenleri public static final'dır!
