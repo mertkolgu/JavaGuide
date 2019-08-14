fffdc5e7b43c2a8fdc8e840cc5cc20a656d8b9ac
mertkolgu <mertkolgu@outlook.com>
2019-08-14 20:27:49 +0300

    method local inner

---
+++ src/com/javase/_47/methodlocalinner/NotesForMethodLocalInner.java
@@ -0,0 +1,35 @@
+package com.myjavase._47.methodlocalinner;
+
+public class NotesForMethodLocalInner {
+
+    public static void main(String[] args) {
+        //
+    }
+
+    private int value = 200;
+
+    void method() {
+
+        // legal değildir.
+        // LocalInner localInner = new LocalInner();
+        // localInner.innerMethod();
+
+        class LocalInner {
+
+            String str = "java rule";
+
+            void innerMethod() {
+                System.out.println(value);
+                // regular inner class'ta olduğu gibi,
+                // outer class'ın private üyelerine erişim sağlayabilir.
+
+                System.out.println(str);
+
+                str = str + "!!!";
+            }
+        }
+
+        LocalInner localInner = new LocalInner();
+        localInner.innerMethod();
+    }
+}
