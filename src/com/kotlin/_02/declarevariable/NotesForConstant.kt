7ef5516b8d89d1e4117b235eb6d365ec6bb587e2
mertkolgu <mertkolgu@outlook.com>
2019-08-18 01:06:49 +0300

    kotlin declare variable

---
+++ src/com/kotlin/_02/declarevariable/NotesForConstant.kt
@@ -0,0 +1,9 @@
+package com.kotlin._02.declarevariable
+
+fun main() {
+
+    val pi: Double = Math.PI   // sadece bir kere değer ataması yapılabilir
+    println(pi)
+}
+
+// sabit değişkenleri tanımlamak için val keyword'ü kullanılır.
