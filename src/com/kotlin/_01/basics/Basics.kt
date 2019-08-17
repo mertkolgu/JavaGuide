f86cc80f8d9a6d6c309aee3d8565ef87364841e7
mertkolgu <mertkolgu@outlook.com>
2019-08-18 00:38:43 +0300

    basics

--- src/com/kotlin/_01/basics/Basics.kt
+++ src/com/kotlin/_01/basics/Basics.kt
@@ -3,6 +3,10 @@ package com.kotlin._01.basics
 fun main() {
     println("Hello world!")
     println("Mert Kolğu")
+
+    println("""Kotlindeki \n işareti bir alt satıra geçmeyi sağlar.""")
+    // burada \n'nin bir alt satıra geçme işlemini yapmasını engellemek için
+    // """\n""" bu şekilde kullanılması lazım.
 }
 
 // javadan farklı olarak ; kullanma zorunluluğu yoktur.
