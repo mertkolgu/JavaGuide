f4ce647970e4a70274c20c33ed14dea5acc05e3a
mertkolgu <mertkolgu@outlook.com>
2019-08-24 23:57:35 +0300

    wrapper

--- src/com/javase/_25/wrapper/NotesForWrapper.java
+++ src/com/javase/_25/wrapper/NotesForWrapper.java
@@ -1,7 +1,27 @@
 package com.javase._25.wrapper;
 
+import java.util.ArrayList;
+
 public class NotesForWrapper {
-    //
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++)
+            arrayList.add(Integer.valueOf(i));  // AutoBoxing
+
+        for (int i = 0; i < arrayList.size(); i++)
+            System.out.println(arrayList.get(i).intValue());    // UnBoxing
+
+        // Java, AutoBoxing ve UnBoxing işlemlerini otomatik olarak kendisi yapıyor.
+
+        for (int i = 0; i < 10; i++)
+            arrayList.add(i);   // Integer.valueOf(i)
+
+        for (int i = 0; i < arrayList.size(); i++)
+            System.out.println(arrayList.get(i));   // arrayList.get(i).intValue()
+    }
 }
 
 // wrapper -> sarmalayan, sarıcı gibi Türkçe anlamları var
