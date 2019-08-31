ad1440566fcf67c34f72ddc07f4543ed55434d01
mertkolgu <mertkolgu@outlook.com>
2019-08-31 18:55:24 +0300

    naming standart

---
+++ src/com/javase/_02/namingstandart/NotesForPackage.java
@@ -0,0 +1,14 @@
+package com.javase._02.namingstandart;
+
+public class NotesForPackage {
+    //
+}
+
+// package isimleri küçük harf ile başlamalı
+// benzersiz bir isim verilmeli
+// projenin web sitesi 'www.mertkolgu.com.tr/test' ise
+// package ismi 'tr.com.mertkolgu.test' olmalı.
+// '-' yerine '_' kullanılmalı
+// package isimleri Java'daki anahtar kelimeleri içeriyorsa veya
+// değişkenlere verme kurallarına aykırıysa '_' ile düzeltme yapılmalı
+// while.xyz.com -> com.xyz._while
